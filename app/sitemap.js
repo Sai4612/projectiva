9e63fa591a586314c1965f1467df1ce6ebb8662d
yedapallysanjanareddy-ai <yedapallysanjanareddy@gmail.com>
2026-04-16 15:37:22 +0530

    fix sitemap export

---
+++ app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://projectiva.in",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://projectiva.in/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://projectiva.in/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://projectiva.in/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
