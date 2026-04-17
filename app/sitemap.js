527bdf34550360912adc255277c7029b71005d66
yedapallysanjanareddy-ai <yedapallysanjanareddy@gmail.com>
2026-04-17 10:56:34 +0530

    fix sitemap properly

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,19 +1,19 @@
 export default function sitemap() {
   return [
     {
-      url: "https://projectiva.in",
+      url: "https://projectiva.sbs",
       lastModified: new Date(),
     },
     {
-      url: "https://projectiva.in/about",
+      url: "https://projectiva.sbs/about",
       lastModified: new Date(),
     },
     {
-      url: "https://projectiva.in/services",
+      url: "https://projectiva.sbs/services",
       lastModified: new Date(),
     },
     {
-      url: "https://projectiva.in/contact",
+      url: "https://projectiva.sbs/contact",
       lastModified: new Date(),
     },
   ];
